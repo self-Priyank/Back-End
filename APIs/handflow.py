a4f244d3d189ef2234fd88bfeb1d5e91d09d7608
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2025-07-21 11:34:03 +0530

    API info. commited

---
+++ APIs/handflow.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import json       # use to read, edit & parse JSON file
+
+# Parsing: converts JSON raw data into structured data based on its format. occurs in json.load() 
+# most cases, reading & parsing are done in single call, reading occurs first (internally) & then parsing
+try:
+    with open("users.json", "r") as f:
+        data = json.load(f)   # type = list of dict.
+except FileNotFoundError:
+    print("Error: 'user.json' file was not found")
+    data = []
+except json.JSONDecodeError:
+    print("Error: 'user.json' file is wrongly formatted")    # Invalid JSON
+    data = []
+except Exception as e:
+    print(f"Unexpected error: {e}")
+    data = []
+# prog. opens file & loads its content by parsing it into list of dict.
+
+class Person(BaseModel):
+    email: str
+    password: str
+
+app = FastAPI()
+
+@app.get("/")    # represent URL
+def read_URL():
+    return "!! Bienvenue !!"
+
+@app.get("/users", response_model=list[Person])
+def get_all_users():
+    return data
+
