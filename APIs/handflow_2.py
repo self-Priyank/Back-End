c72833944431a00982e16165224ad1dbd7ff3aea
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2025-08-07 13:18:13 +0530

    MongoDB-API experiment 4 success

--- APIs/handflow_2.py
+++ APIs/handflow_2.py
@@ -124,6 +124,6 @@ def pinning_task(usr_nm: str):
             raise HTTPException(status_code=404, detail=f"No task found with username {usr_nm}")
         p_docs = process_data(docs)
         task_coll.update_one({"username": usr_nm}, {"$set": {"is_pinned": True}})   
-        return {"message": f"Task ID {str(p_docs['id'])} is pinned"} 
+        return {"message": f"Task ID {p_docs['id']} is pinned"} 
     except PyMongoError:
         raise HTTPException(status_code=500, detail="database server error")
